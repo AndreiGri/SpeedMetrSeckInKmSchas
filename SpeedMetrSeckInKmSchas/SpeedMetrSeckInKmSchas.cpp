03c3833154fa4be141a49345e89dd9503c6ee51d
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-05 22:00:21 +0500

    Добавьте файлы проекта.

---
+++ SpeedMetrSeckInKmSchas/SpeedMetrSeckInKmSchas.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+double MetrSeckInKmSchas(int sp) {
+    double kmSchas = (double)sp / 1000 * 60 * 60;
+    return kmSchas;
+}
+
+int main()
+{
+    system("chcp 1251 > nul");
+    double speed;
+    cout << "Введите скорость в метрах в секунду: ";
+    cin >> speed;
+    cout << "Скорость " << MetrSeckInKmSchas(speed) << " километров в час." << endl;
+    system("pause > nul");
+    return 0;
+}
